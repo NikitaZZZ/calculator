843a82ed59dad6eb8a9bb75cebb952cfdc63f605
Nikita <zininnikita309@gmail.com>
2022-06-19 10:34:16 +0900

    optimized code and fixed bugs

--- calc.js
+++ calc.js
@@ -7,85 +7,55 @@ let isNewNumber = true;
 currentOperation = '';
 
 function innerCalc(num) {
-  if (Number.isInteger(num)) {
-    if (isNewNumber) {
-      array_nums.push(`${num}`);
-      isNewNumber = false;
-    } else if (!isNewNumber) {
-      array_nums[index] = `${array_nums[index]}${num}`;
-    }
+  if (isNewNumber) {
+    array_nums.push(`${num}`);
+    isNewNumber = false;
+  } else if (!isNewNumber) {
+    array_nums[index] = `${array_nums[index]}${num}`;
   }
 
   innerCalcElem.innerHTML += num;
 }
 
-function reload(result) {
+function resultOuptut(result) {
   array_nums = [result];
   innerCalcElem.innerHTML = array_nums;
 }
 
-function divide() {
-  for (let i = 0; i < array_nums.length; i++) {
-    const num1 = parseInt(array_nums[i]);
-    const num2 = parseInt(array_nums[i + 1]);
+function calculation() {
+  let result = 0;
 
-    result = num1 / num2;
-    reload(result);
+  const num1 = parseInt(array_nums[0]);
+  const num2 = parseInt(array_nums[1]);
+
+  switch (currentOperation) {
+    case '+':
+      result = num1 + num2;
+      break;
+    case '-':
+      result = num1 - num2;
+      break;
+    case '*':
+      result = num1 * num2;
+      break;
+    case '/':
+      result = num1 / num2;
+      break;
   }
-}
 
-function multiply() {
-  for (let i = 0; i < array_nums.length; i++) {
-    const num1 = parseInt(array_nums[i]);
-    const num2 = parseInt(array_nums[i + 1]);
-
-    result = num1 * num1;
-    reload(result);
-  }
-}
-
-function fold() {
-  for (let i = 0; i < array_nums.length; i++) {
-    const num1 = parseInt(array_nums[i]);
-    const num2 = parseInt(array_nums[i + 1]);
-
-    result = num1 - num1;
-    reload(result);
-  }
-}
-
-function subtract() {
-  for (let i = 0; i < array_nums.length; i++) {
-    const num1 = parseInt(array_nums[i]);
-    const num2 = parseInt(array_nums[i + 1]);
-
-    result = num1 + num1;
-    reload(result);
-  }
+  resultOuptut(result);
 }
 
 function changeOperations(operation) {
-  innerCalcElem.innerHTML += operation;
-  currentOperation = operation;
-  isNewNumber = true;
+  if (array_nums.length < 2) {
+    innerCalcElem.innerHTML += ` ${operation} `;
+    currentOperation = operation;
+    isNewNumber = true;
 
-  index++;
-}
-
-function equal() {
-  switch (currentOperation) {
-    case '+':
-      subtract();
-      break;
-    case '-':
-      fold();
-      break;
-    case '*':
-      multiply();
-      break;
-    case '/':
-      divide();
-      break;
+    index++;
+  } else {
+    calculation();
+    changeOperations(operation);
   }
 }
 
